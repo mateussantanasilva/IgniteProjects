4c50f49e579a11d8cd1ab56fb60d9f20ea22705e
mateussantanasilva <santanasilva1778@gmail.com>
2023-07-03 19:27:17 -0300

    ✨ feat: Tarefas dinâmicas

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,22 +1,46 @@
 'use client'
-
 import { Check, Trash } from '@phosphor-icons/react'
 import styles from './Task.module.css'
 
-export function Task() {
+interface TaskTemplate {
+  id: string
+  content: string
+  onChangeTaskStatus: (id: string) => void
+  onDeleteTask: (id: string) => void
+}
+
+export function Task({
+  id,
+  content,
+  onChangeTaskStatus,
+  onDeleteTask,
+}: TaskTemplate) {
+  function handleChangeTaskStatus() {
+    onChangeTaskStatus(id)
+  }
+  function handleDeleteTask() {
+    onDeleteTask(id)
+  }
+
   return (
     <article className={styles.task}>
-      <input type="checkbox" name="isDone" id="isDone" />
-      <label className={styles.checkbox} htmlFor="isDone">
+      <input
+        type="checkbox"
+        name="isDone"
+        id={id}
+        onChange={handleChangeTaskStatus}
+      />
+      <label className={styles.checkbox} htmlFor={id}>
         <Check weight="bold" size={'.7rem'} />
       </label>
 
-      <p>
-        Integer urna interdum massa libero auctor neque turpis turpis semper.
-        Duis vel sed fames integer.
-      </p>
+      <p>{content}</p>
 
-      <button className={styles.deleteTask}>
+      <button
+        className={styles.deleteTask}
+        onClick={handleDeleteTask}
+        title="Deletar tarefa"
+      >
         <Trash size={'1.1rem'} />
       </button>
     </article>
