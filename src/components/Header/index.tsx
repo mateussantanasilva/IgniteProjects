c6e19ef8819926edc916ec15f3fdf98d84833fde
mateussantanasilva <santanasilva1778@gmail.com>
2023-07-05 15:48:21 -0300

    💄 feat: Componentes criados

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom'
+import { Timer, Scroll } from '@phosphor-icons/react'
+
+import logoIgnite from '../../../public/logo-ignite.svg'
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img
+        src={logoIgnite}
+        alt="Dois triângulos verdes apontando para cima representando o logo do ignite"
+      />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={'2.4rem'} />
+        </NavLink>
+        <NavLink to="/historico" title="Histórico">
+          <Scroll size={'2.4rem'} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
