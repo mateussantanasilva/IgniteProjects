8fadb35d1acb954f75d374c5d6c8a9b0a8e65d30
mateussantanasilva <santanasilva1778@gmail.com>
2023-07-05 15:47:21 -0300

    ✨ feat: Rotas para as páginas criadas

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,12 +16,12 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
-    border-radius: 4px;
   }
 
   body {
     background: ${(props) => props.theme['gray-900']};
     color: ${(props) => props.theme['gray-300']};
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
