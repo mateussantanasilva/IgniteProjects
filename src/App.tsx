8527e509c01a851369fdc765163e55dde936b8e4
mateussantanasilva <santanasilva1778@gmail.com>
2023-07-09 17:40:29 -0300

    ✨ feat: Recurso de alterar tema adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { ThemeProvider } from 'styled-components'
+// import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
 
-import { defaultTheme } from './styles/themes/default'
+// import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
 import { CyclesContextProvider } from './contexts/CyclesContext'
+import { ThemeContextProvider } from './contexts/ThemeContext'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeContextProvider>
       <BrowserRouter>
         <CyclesContextProvider>
           <Router />
         </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
-    </ThemeProvider>
+    </ThemeContextProvider>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,31 @@
+import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
-import { Timer, Scroll } from '@phosphor-icons/react'
+import { ThemeContext } from '../../contexts/ThemeContext'
 
+import { Timer, Scroll, Sun, Moon } from '@phosphor-icons/react'
 import logoIgnite from '../../../public/logo-ignite.svg'
-import { HeaderContainer } from './styles'
+import { ButtonContainer, HeaderContainer } from './styles'
 
 export function Header() {
+  const { isDefaultTheme, toggleTheme } = useContext(ThemeContext)
+
+  function handleToggleTheme() {
+    toggleTheme()
+  }
+
   return (
     <HeaderContainer>
-      <NavLink to="/" title="Home">
-        <img
-          src={logoIgnite}
-          alt="Dois triângulos verdes apontando para cima representando o logo do ignite"
-        />
-      </NavLink>
+      <div>
+        <NavLink to="/" title="Home">
+          <img
+            src={logoIgnite}
+            alt="Dois triângulos verdes apontando para cima representando o logo do ignite"
+          />
+        </NavLink>
+        <ButtonContainer title="Alterar tema" onClick={handleToggleTheme}>
+          {isDefaultTheme ? <Sun size={'2.4rem'} /> : <Moon size={'2.4rem'} />}
+        </ButtonContainer>
+      </div>
 
       <nav>
         <NavLink to="/" title="Timer">
