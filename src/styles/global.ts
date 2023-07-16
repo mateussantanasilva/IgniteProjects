4e67cbfe819103f1dd28a1b48ff345de55698215
mateussantanasilva <santanasilva1778@gmail.com>
2023-07-16 12:53:36 -0300

    🎉 Projeto DT Money iniciado

---
+++ src/styles/global.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    font-size: 62.5%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: none;
+    text-decoration: none;
+    outline: 0;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+  }
+
+  body {
+    background: ${(props) => props.theme['gray-800']};
+    color: ${(props) => props.theme['gray-100']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1.6rem Roboto, sans-serif; 
+    line-height: 160%;
+  }
+`
