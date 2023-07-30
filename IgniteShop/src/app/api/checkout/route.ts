abd8bf80cd3b7d1f2def4443e957c0590f75cdf9
mateussantanasilva <santanasilva1778@gmail.com>
2023-07-30 16:18:34 -0300

    ✨ feat: Criação de sessão de checkout atualizada

--- IgniteShop/src/app/api/checkout/route.ts
+++ IgniteShop/src/app/api/checkout/route.ts
@@ -1,30 +1,31 @@
+import { ProductType } from '@/app/page'
 import { stripe } from '@/libs/stripe'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
-  const { priceId } = await request.json() // request.body
+  const { cartItems } = await request.json() // request.body
 
-  if (!priceId) {
-    return NextResponse.json({ status: 400, error: 'Price not found' })
+  if (!cartItems) {
+    return NextResponse.json({ status: 400, error: 'Cart items not found' })
   }
+
+  const lineItems = cartItems.map((item: ProductType) => {
+    return { price: item.defaultPriceId, quantity: 1 }
+  })
 
   const successUrl = `${process.env.NEXT_PUBLIC_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_PUBLIC_URL}/`
 
   const checkoutSession = await stripe.checkout.sessions.create({
     mode: 'payment',
-    line_items: [
-      {
-        price: priceId, // item identifier
-        quantity: 1,
-      },
-    ],
+    line_items: lineItems,
     success_url: successUrl,
     cancel_url: cancelUrl,
   })
 
   return NextResponse.json({
     status: 201,
+    sessionId: checkoutSession.id,
     checkoutUrl: checkoutSession.url,
   })
 }
